315116bc3c5635f8c9d261321281e048c8de6186
lizhijian <lizhijian@aobi.com>
2024-06-27 11:54:45 +0800

    feat：【包体工程】 提交工程

---
+++ TestPackage/Assets/TestRes/Runtime/ResourceManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace LingDu
+{
+    public class ResourceManager
+    {
+        public static void GetPath()
+        {
+            Debug.LogError("this is Tool get resource ");
+        }
+    }
+
+
+}
